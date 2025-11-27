9c12f9637c1fc69accf4941a7d617bff66bf6353
Nafismaulanaa <23523014@students.uii.ac.id>
2025-11-27 22:07:26 +0700

    cek error

--- app/src/main/java/com/example/testhydromate/AppNavigation.kt
+++ app/src/main/java/com/example/testhydromate/AppNavigation.kt
@@ -21,7 +21,7 @@ fun AppNavigation() {
             route = Screen.Splash.route,
             exitTransition = { fadeOut(tween(1000)) }
         ) {
-            SplashScreen(
+            SplashScree(
                 onFinished = {
                     navController.navigate(Screen.Login.route) {
                         popUpTo(Screen.Splash.route) { inclusive = true }
